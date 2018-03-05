f85e6bc3fb42f6ef01912304a056c4e1135fb90d
unknown <liyne@ufsoft.com.cn>
2018-03-05 19:18:08 +0800

    first commit

---
+++ app/components/hello.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './hello.less'
+
+let Hello =React.ceateClass({
+	render(){
+		 return (
+                <div className="hello-compoment">
+                   hekllllll
+                </div>
+		 	);
+	}
+})
+export default Hello;
